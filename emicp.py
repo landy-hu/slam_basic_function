a906772d2d7e818dbed53d959dfd0dc5897ff65b
landy-hu <18818218629@163.com>
2021-08-03 16:39:48 +0800

    360bin and awareness of the height

--- emicp.py
+++ emicp.py
@@ -1,6 +1,6 @@
 import numpy as np
 from scipy.optimize import least_squares
-from tools import rotationMatrixToEulerAngles, eulerAnglesToRotationMatrix, show_pair_pc, rigid_transform, downsample, points_denoise
+from tools import rotationMatrixToEulerAngles, eulerAnglesToRotationMatrix, show_pair_pc, rigid_transform, downsample, points_denoise, points_distance
 from sklearn.neighbors import NearestNeighbors
 import math
 import concurrent.futures
@@ -12,94 +12,38 @@ class robust_icp():
     def __init__(self):
         self.delta = 0.02
         self.rotm_axis = "y"
+        self.height = 1
 
     def em_icp(self, source, target):
-        source = self.downsample(source, voxel_size=0.06)
-        target = self.downsample(target, voxel_size=0.06)
+        R = np.eye(3)
+        t = np.zeros(3)
+        s = 1
 
-        tar_scale = np.max(np.linalg.norm(target - np.min(target, axis=0), axis=1))
-        src_scale = np.max(np.linalg.norm(source - np.min(source, axis=0), axis=1))
+        tar_scale = self.estimate_length(target)
+        src_scale = self.estimate_length(source)
         ratio = tar_scale / src_scale
-        src = source * ratio
-        s = ratio
-        mean_src = np.mean(src, axis=0)
-        mean_tar = np.mean(target, axis=0)
-        t = - mean_src + mean_tar
-        src = s*source+t
-        #
-        v = np.zeros(4)
-        res_1 = least_squares(self.cost_function, v, args=(src, target))
-        v = res_1.x
-        error = res_1.cost
-        R = self.generate_rotm(v[0])
-        t = v[1:4] + R.dot(t)
-        # show_pair_pc(source, target)
-        #
-        # R, t, s, error = self.iterative0(source, target)
-
-        src = ((s*source).dot(R.transpose()) + t)
-        # show_pair_pc(src, target)
-        tar_scale = np.max(np.linalg.norm(target - np.min(target, axis=0), axis=1))
-        src_scale = np.max(np.linalg.norm(src - np.min(src, axis=0), axis=1))
-        ratio = tar_scale / src_scale
-        src = src * ratio
-        v = np.zeros(4)
-        v[:1] = 1.0
-        res_1 = least_squares(self.scale_funtion, v, args=(src, target))
-        error = res_1.cost
-        x = res_1.x
-        s = s*ratio*x[:1]
-        t = ratio*x[:1]*t + x[1:]
-
-        # show_pair_pc(src, target)
+        ratio[1] = 1
+        s = s*ratio
+        src = s*source + t
+        error = self._distance(src, target)
 
         T = np.identity(4)
         T[:3, :3] = R
         T[:3, 3] = t
-
 
         return T, error, s
 
     def iteratve_icp(self, source, target):
         mean_source = np.mean(source, axis=0)
         mean_target = np.mean(target, axis=0)
-        src = source - mean_source
-        tar = target - mean_target
-        R, t, s, error = self.iterative(src, tar)
-        t = t + mean_target - R.dot(s*mean_source)
+        # source = source - mean_source
+        # target = target - mean_target
+        R, t, s, error = self.iterative(source, target)
+        # t = t + mean_target - R.dot(s*mean_source)
         T = np.identity(4)
         T[:3, :3] = R
         T[:3, 3] = t
         return T, error, s
-
-    def SVD_T(self, source, target, iter=40):
-
-        mean_s = np.mean(source, axis=0)
-        mean_t = np.mean(target, axis=0)
-        source_m = source - mean_s
-        target_m = target - mean_t
-
-        s = np.mean(np.linalg.norm(source_m * target_m, axis=1) / np.linalg.norm(source_m * source_m, axis=1))
-        v = 20
-        s_max = 1 + 1 / (v + iter)
-        s_min = 1 - 1 / (v + iter)
-        if s > s_max:
-            s = s_max
-        elif s < s_min:
-            s = s_min
-        source_m[:, 1] = 0
-        target_m[:, 1] = 0
-        S = target_m.transpose().dot(s*source_m)
-        U, A, V = np.linalg.svd(S)
-        R = U.dot(V)
-
-        if np.linalg.det(R)<0:
-            R = -1*R
-        t = mean_t - s*R.dot(mean_s)
-        src = s * source.dot(R.transpose()) + t
-        # show_pair_pc(src, target)
-
-        return R, t, s
 
     def SVD_T_1(self, source, target, iter):
         mean_s = np.mean(source, axis=0)
@@ -108,6 +52,7 @@ class robust_icp():
         target_m = target - mean_t
 
         s = self.estimate_scale(source_m, target_m, iter)
+        s[1] = 1
         source_m[:, 1] = 0
         target_m[:, 1] = 0
         S = target_m.transpose().dot(source_m.dot(np.diag(s)))
@@ -150,57 +95,25 @@ class robust_icp():
         pt_scale = np.max(pt, axis=0)
         return pt_scale
 
-    def iterative0(self, source, target, iteration=50):
-        R = np.eye(3)
-        t = np.zeros(3)
-        s = 1
-        nbrs1 = NearestNeighbors(algorithm='kd_tree', n_neighbors=1).fit(target)
-        for iter in range(iteration):
-            src = (s*source).dot(R.transpose()) + t
-            # show_pair_pc(src, target)
-            tar_scale = np.max(np.linalg.norm(target[:,[0,2]] - np.min(target[:,[0,2]], axis=0), axis=1))
-            src_scale = np.max(np.linalg.norm(src[:,[0,2]] - np.min(src[:,[0,2]], axis=0), axis=1))
-            ratio = tar_scale / src_scale
-            src = src*ratio
-
-            # show_pair_pc(src, target)
-            distance1, indices = nbrs1.kneighbors(src)
-
-
-            tar = target[indices[:, 0], :3]
-            idx = self.first_k(distance1, 0.9)
-            tar = tar[idx, :]
-            src = src[idx, :]
-            # show_pair_pc(src, tar)
-            R_new, t_new, s_new = self.SVD_T(src, tar, iter)
-
-            R = R_new.dot(R)
-            t = s_new*R_new.dot(t) + t_new
-            s = s*s_new*ratio
-
-            U, A, V = np.linalg.svd(R)
-            R = U.dot(V)
-            if np.linalg.det(R) < 0:
-                R = -1 * R
-        error = self._distance(src, target, 1)
-        return R, t, s, error
-
     def iterative(self, source, target,iter=50):
         R = np.eye(3)
         t = np.zeros(3)
         s = 1
-        error = 100
         for iter in range(iter):
             src = ((s*source).dot(R.transpose()) + t)
             tar_scale = self.estimate_length(target)
             src_scale = self.estimate_length(src)
+            # src_scale[1] = self.height
             ratio = tar_scale / src_scale
+            ratio[1] = 1
+
             src *= ratio
 
             nbrs1 = NearestNeighbors(algorithm='kd_tree', n_neighbors=1).fit(target)
             distance1, indices = nbrs1.kneighbors(src)
 
             tar = target[indices[:, 0], :]
+
             R_new, t_new, s_new = self.SVD_T_1(src, tar, iter)
 
             R = R_new.dot(R)
@@ -217,14 +130,34 @@ class robust_icp():
     def _distance(self, source, target, k=0.9):
         nbrs1 = NearestNeighbors(algorithm='kd_tree', n_neighbors=1).fit(source)
         distance1, _ = nbrs1.kneighbors(target)
-        errors = np.mean(distance1)
+        errors = np.mean(self.huber_loss(distance1))
 
-        nbrs1 = NearestNeighbors(algorithm='kd_tree', n_neighbors=1).fit(target)
-        distance1, _ = nbrs1.kneighbors(source)
-        idx = self.first_k(distance1, k)
-        errors += np.mean(distance1[idx, 0])
+        # nbrs1 = NearestNeighbors(algorithm='kd_tree', n_neighbors=1).fit(target)
+        # distance1, _ = nbrs1.kneighbors(source)
+        # idx = self.first_k(distance1, k)
+        # errors += 0.7*np.mean(distance1[idx, 0])
+        # nbrs1 = NearestNeighbors(algorithm='kd_tree', n_neighbors=1).fit(p2)
+        # distance11, indices11 = nbrs1.kneighbors(p1)
+        # nbrs2 = NearestNeighbors(algorithm='kd_tree', n_neighbors=1).fit(p1)
+        # distance22, indices22 = nbrs2.kneighbors(p2)
+        # indices11 = indices11[distance11[:, 0] < 0.1, 0]
+        # indices22 = indices22[distance22[:, 0] < 0.1, 0]
+        # ratio = (indices11.shape[0] + indices22.shape[0]) / ((distance11.shape[0] + distance22.shape[0]))
+        # res = (np.mean(distance11[:, 0]) + ratio * np.mean(distance22[:, 0]) )/(1+ratio)
         return errors
-
+    def refine_result(self, src, tar):
+        src = self._resample(src,500)
+        tar = self._resample(tar,500)
+        v = np.zeros(4)
+        res_1 = least_squares(self.cost_function, v, args=(src, tar), ftol=1e-3, xtol=1e-3, gtol=1e-3)
+        v = res_1.x
+        error = res_1.cost
+        R = self.generate_rotm(v[0])
+        t = v[1:4]
+        T = np.identity(4)
+        T[:3, :3] = R
+        T[:3, 3] = t
+        return T, error
     def cost_function(self, x, *args):
         source = args[0]
         target = args[1]
@@ -239,6 +172,17 @@ class robust_icp():
         errors = self.huber_loss(distance1[:,0])
 
         return errors
+    def _resample(self, points, k):
+        # k = self.num_for_registration
+        if k <= points.shape[0]:
+            rand_idxs = np.random.choice(points.shape[0], k, replace=False)
+            return points[rand_idxs, :]
+        elif points.shape[0] == k:
+            return points
+        else:
+            rand_idxs = np.concatenate([np.random.choice(points.shape[0], points.shape[0], replace=False),
+                                        np.random.choice(points.shape[0], k - points.shape[0], replace=True)])
+            return points[rand_idxs, :]
 
     def registration(self, x, *args):
         source = args[0]
@@ -257,18 +201,18 @@ class robust_icp():
         t = x[1:]
         s = x[:1]
         src = s*source + t
-        nbrs1 = NearestNeighbors(algorithm='kd_tree', n_neighbors=1).fit(src)
-        distance1, indices = nbrs1.kneighbors(target)
-        errors = self.huber_loss(distance1[:, 0])
-
         nbrs1 = NearestNeighbors(algorithm='kd_tree', n_neighbors=1).fit(target)
         distance1, indices = nbrs1.kneighbors(src)
-        distance1 = self.huber_loss(distance1, 0.15)
-        errors = np.concatenate((distance1[:, 0], errors), axis=0)
+        errors = self.huber_loss(distance1[:, 0])
+
+        nbrs1 = NearestNeighbors(algorithm='kd_tree', n_neighbors=1).fit(src)
+        distance1, indices = nbrs1.kneighbors(target)
+        distance1 = self.huber_loss(distance1)
+        errors = np.concatenate((0.55*distance1[:, 0], errors), axis=0)
 
         return errors
 
-    def huber_loss(self, errors, delta=0.08):
+    def huber_loss(self, errors, delta=0.05):
         # delta = 0.08
         temp = errors
         errors[temp <= delta] = np.sqrt(0.5) * errors[temp <= delta]
@@ -295,7 +239,7 @@ class global_em_icp(robust_icp):
          --error: registration error
         """
         gaps = 2*math.pi/steps*57.3
-        self.rot_deg_list = [gaps*i + gaps/2  for i in range(steps)]
+        self.rot_deg_list = [gaps*i + gaps/2 for i in range(steps)]
         self.rotm_axis = rotm_axis
         self.flag = flag
         # if flag == 1:
@@ -303,17 +247,6 @@ class global_em_icp(robust_icp):
         # else:
         self.num_for_registration = 800
 
-    def _resample(self, points):
-        k = self.num_for_registration
-        if k <= points.shape[0]:
-            rand_idxs = np.random.choice(points.shape[0], k, replace=False)
-            return points[rand_idxs, :]
-        elif points.shape[0] == k:
-            return points
-        else:
-            rand_idxs = np.concatenate([np.random.choice(points.shape[0], points.shape[0], replace=False),
-                                        np.random.choice(points.shape[0], k - points.shape[0], replace=True)])
-            return points[rand_idxs, :]
 
     def downsample(self, ptc, voxel_size):
         pcd = o3d.geometry.PointCloud()
@@ -329,15 +262,11 @@ class global_em_icp(robust_icp):
             ptc = np.array(downpcd.points)
             return ptc
 
-    def run(self, source, target):
-
+    def run(self, source, target, object_height):
+        self.height = object_height
 
         self.source_down = source
         self.target_down = target
-        # self.t
-        # self.tar_scale =  np.max(np.linalg.norm(self.target_down -np.min(self.target_down, axis=0), axis=1))
-        # self.source = self.source
-        # self.target  = self.target
 
         dist_error = []
         T_set = []
@@ -349,46 +278,39 @@ class global_em_icp(robust_icp):
             dist_error.append(result[1])
             s_set.append(result[2])
         executor.shutdown(wait=True)
-        #
+
         # for i in range(len(self.rot_deg_list)):
         #     result = self.multi_process(self.rot_deg_list[i])
         #     T_set.append(result[0])
         #     dist_error.append(result[1])
         #     s_set.append(result[2])
-            # trans = result[0]
-            # print(result[1])
-            # show_pair_pc(rigid_transform(self.source_down, trans), self.target_down)
 
         idx = np.argmin(dist_error)
         trans = T_set[idx]
         error = dist_error[idx]
+        # tar_scale = np.max(np.linalg.norm(self.target_down - np.min(self.target_down, axis=0), axis=1))  # [:,[0,2]]
+        # error /= tar_scale
+        # src = rigid_transform(self.source_down, trans)
+        # trans_new, error = self.refine_result(src, self.target_down)
+        # trans = trans_new.dot(trans)
         tar_scale = np.max(np.linalg.norm(self.target_down - np.min(self.target_down, axis=0), axis=1))  # [:,[0,2]]
         error /= tar_scale
-        # src_trans = rigid_transform(self.source_down, trans)
-        # R, t, s, error = self.iterative(src_trans, self.target_down, iter=500)
-        # show_pair_pc(rigid_transform(self.source_down,trans), self.target_down)
-        # trans[:3,:3] = s*R.dot(trans[:3,:3])
-        # trans[:3,3] = s*R.dot(trans[:3,3]) + t
-        # s = s_set[idx]
-
-
-
         return trans, error
 
     def multi_process(self, rot_deg):
 
         tar_mean = np.max(self.target_down, axis=0)
         tar = self.target_down - tar_mean
-        tar_scale = np.max(np.linalg.norm(tar[:,[0,2]] -np.min(tar[:,[0,2]], axis=0), axis=1)) #[:,[0,2]]
 
         rotm = self.generate_rotm(rot_deg)
         src = self.source_down.dot(rotm.transpose())
         src_mean = np.max(src, axis=0)
         src = src - src_mean
-        src_scale = np.max(np.linalg.norm(src[:,[0,2]]  - np.min(src[:,[0,2]], axis=0), axis=1))
+        tar_scale = self.estimate_length(tar)
+        src_scale = self.estimate_length(src)
+        src_scale[1] = self.height
         ratio = tar_scale / src_scale
         src = src * ratio
-        # show_pair_pc(src, tar)
         trans, error, s = self.em_icp(src, tar)
         s *= ratio
         trans[:3,  3] = -(trans[:3, :3]*s).dot(src_mean) + trans[:3, 3] + tar_mean
@@ -398,10 +320,7 @@ class global_em_icp(robust_icp):
 
 
 if __name__=="__main__":
-    source = np.random.random((1024, 3))
-    R = eulerAnglesToRotationMatrix([0, 3.14/2, 0])
-    target = source.dot(R.transpose())
-    em_icp = global_em_icp(12)
-    trans, error = em_icp.run(source, target)
-    show_pair_pc(target, rigid_transform(source, trans) + np.array([0,0.1,1]))
+    em_icp = global_em_icp(360)
+    object_height = np.max(cad_model_gt[:, 1]) - np.min(cad_model_gt[:, 1])
+    trans, errors = em_icp.run(object.copy(), cad_model.copy(), object_height)
 
