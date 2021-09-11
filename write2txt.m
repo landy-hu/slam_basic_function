160dbba3b18f5ea684f1fcc256e1b9f5ea4b6791
landy-hu <18818218629@163.com>
2021-09-11 13:34:25 +0800

    Add files via upload

---
+++ write2txt.m
@@ -0,0 +1,14 @@
+function write2txt(filename, pt)
+fid = fopen(filename,'w+');
+[n,m]=size(pt);
+fprintf(fid,'%d\n',n);
+
+for i=1:n
+    for j=1:m
+        fprintf(fid,'%.4f  ', pt(i,j));
+    end
+            fprintf(fid,'\n');
+end
+
+fclose(fid);
+end
